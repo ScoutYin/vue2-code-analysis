94961aa62d18a42798264c43e38e4bfbb27da436
ScoutYin <scoutyin.1900@gmail.com>
2022-06-21 01:54:02 +0800

    add: 基础工具函数分析

--- code/src/shared/util.js
+++ code/src/shared/util.js
@@ -150,33 +150,42 @@ export function toNumber (val: string): number | string {
 /**
  * Make a map and return a function for checking if a key
  * is in that map.
+ * 创建一个闭包对象，保存映射，然后返回一个函数用于判断某个 key 是否存在该 map 中
  */
 export function makeMap (
   str: string,
   expectsLowerCase?: boolean
 ): (key: string) => true | void {
+  // 创建一个闭包对象
   const map = Object.create(null)
+  // 通过 ‘,’ 来分割字符串，将分割得到的每个子串作为 key
   const list: Array<string> = str.split(',')
   for (let i = 0; i < list.length; i++) {
     map[list[i]] = true
   }
   return expectsLowerCase
+    // 是否转成全小写的key进行判断
     ? val => map[val.toLowerCase()]
     : val => map[val]
 }
 
 /**
  * Check if a tag is a built-in tag.
+ * 判断一个标签是否为 Vue 框架内置标签
+ * 就是判断是否为 slot 和 component 这两个标签
  */
 export const isBuiltInTag = makeMap('slot,component', true)
 
 /**
  * Check if an attribute is a reserved attribute.
+ * 判断 html 标签上某个 attribute 属性是否为 Vue 内部保留的标签属性
+ * Vue 内部保留的标签属性包括：key, ref, slot, slot-scope, is
  */
 export const isReservedAttribute = makeMap('key,ref,slot,slot-scope,is')
 
 /**
  * Remove an item from an array.
+ * 从数组中移除某个元素
  */
 export function remove (arr: Array<any>, item: any): Array<any> | void {
   if (arr.length) {
@@ -189,6 +198,8 @@ export function remove (arr: Array<any>, item: any): Array<any> | void {
 
 /**
  * Check whether an object has the property.
+ * 判断一个属性是否存在于指定对象的自身属性中
+ * 通过 Object.prototype.hasOwnProperty 来判断
  */
 const hasOwnProperty = Object.prototype.hasOwnProperty
 export function hasOwn (obj: Object | Array<*>, key: string): boolean {
@@ -197,17 +208,24 @@ export function hasOwn (obj: Object | Array<*>, key: string): boolean {
 
 /**
  * Create a cached version of a pure function.
+ * 创建一个带执行结果缓存的函数
  */
 export function cached<F: Function> (fn: F): F {
+  // 创建一个闭包缓存对象
   const cache = Object.create(null)
+  // 返回一个带执行结果缓存的函数
   return (function cachedFn (str: string) {
+    // 检查缓存
     const hit = cache[str]
+    // 如果命中缓存，则直接返回命中的缓存，否则才执行 fn，并将 fn 的执行结果进行缓存
     return hit || (cache[str] = fn(str))
   }: any)
 }
 
 /**
  * Camelize a hyphen-delimited string.
+ * 将一个连字符（-）分隔的字符串转成小驼峰格式
+ * camel-case -> camelCase
  */
 const camelizeRE = /-(\w)/g
 export const camelize = cached((str: string): string => {
@@ -216,6 +234,7 @@ export const camelize = cached((str: string): string => {
 
 /**
  * Capitalize a string.
+ * 将一个字符串的首字母大写
  */
 export const capitalize = cached((str: string): string => {
   return str.charAt(0).toUpperCase() + str.slice(1)
@@ -223,6 +242,8 @@ export const capitalize = cached((str: string): string => {
 
 /**
  * Hyphenate a camelCase string.
+ * 将小驼峰的字符串转换成连字符连接的字符串
+ * camelCase -> camel-case
  */
 const hyphenateRE = /\B([A-Z])/g
 export const hyphenate = cached((str: string): string => {
@@ -236,7 +257,7 @@ export const hyphenate = cached((str: string): string => {
  * But removing it would mean breaking code that was able to run in
  * PhantomJS 1.x, so this must be kept for backward compatibility.
  */
-
+// Function.prototype.bind 向后兼容的 polyfill
 /* istanbul ignore next */
 function polyfillBind (fn: Function, ctx: Object): Function {
   function boundFn (a) {
@@ -252,6 +273,7 @@ function polyfillBind (fn: Function, ctx: Object): Function {
   return boundFn
 }
 
+// 原生的 Function.prototype.bind
 function nativeBind (fn: Function, ctx: Object): Function {
   return fn.bind(ctx)
 }
@@ -262,8 +284,10 @@ export const bind = Function.prototype.bind
 
 /**
  * Convert an Array-like object to a real Array.
+ * 将一个类似数组对象转成真的数组类型
  */
 export function toArray (list: any, start?: number): Array<any> {
+  // start 指定转换的起点索引
   start = start || 0
   let i = list.length - start
   const ret: Array<any> = new Array(i)
@@ -275,9 +299,11 @@ export function toArray (list: any, start?: number): Array<any> {
 
 /**
  * Mix properties into target object.
+ * 将 _from 对象中的 key-value 复制到 to 对象中
  */
 export function extend (to: Object, _from: ?Object): Object {
   for (const key in _from) {
+    // 如果存在同名的 key，会被 _from 中的覆盖
     to[key] = _from[key]
   }
   return to
@@ -285,11 +311,13 @@ export function extend (to: Object, _from: ?Object): Object {
 
 /**
  * Merge an Array of Objects into a single Object.
+ * 将一个数组中的子项（Object）key-value 合并到一个对象中
  */
 export function toObject (arr: Array<any>): Object {
   const res = {}
   for (let i = 0; i < arr.length; i++) {
     if (arr[i]) {
+      // 将 arr[i] 对象中的 key-value 复制到 res 对象中
       extend(res, arr[i])
     }
   }
@@ -302,11 +330,13 @@ export function toObject (arr: Array<any>): Object {
  * Perform no operation.
  * Stubbing args to make Flow happy without leaving useless transpiled code
  * with ...rest (https://flow.org/blog/2017/05/07/Strict-Function-Call-Arity/).
+ * 一个没有执行逻辑的空函数，啥也不干的函数
  */
 export function noop (a?: any, b?: any, c?: any) {}
 
 /**
  * Always return false.
+ * 一个总是返回 false 的函数
  */
 export const no = (a?: any, b?: any, c?: any) => false
 
@@ -314,6 +344,7 @@ export const no = (a?: any, b?: any, c?: any) => false
 
 /**
  * Return the same value.
+ * 一个返回函数输入值的函数
  */
 export const identity = (_: any) => _
 
@@ -329,24 +360,30 @@ export function genStaticKeys (modules: Array<ModuleOptions>): string {
 /**
  * Check if two values are loosely equal - that is,
  * if they are plain objects, do they have the same shape?
+ * 判断两个值是否大致相等，也就是看起来相等，
+ * 即如果两个都是对象，则看它们的形状、结构上看起来是否一样，而不管它们是否为同一个引用地址
  */
 export function looseEqual (a: any, b: any): boolean {
   if (a === b) return true
   const isObjectA = isObject(a)
   const isObjectB = isObject(b)
+  // 如果两个都是 Object
   if (isObjectA && isObjectB) {
     try {
       const isArrayA = Array.isArray(a)
       const isArrayB = Array.isArray(b)
       if (isArrayA && isArrayB) {
+        // 如果都是数组，判断长度及对应的每项是否同样 looseEqual
         return a.length === b.length && a.every((e, i) => {
           return looseEqual(e, b[i])
         })
       } else if (a instanceof Date && b instanceof Date) {
+        // 如果都是 Date 类型，则判断它们的时间戳是否相等
         return a.getTime() === b.getTime()
       } else if (!isArrayA && !isArrayB) {
         const keysA = Object.keys(a)
         const keysB = Object.keys(b)
+        // 如果两个都不是数组，则判断它们的 key 的个数及相应的每个键值是否 looseEqual
         return keysA.length === keysB.length && keysA.every(key => {
           return looseEqual(a[key], b[key])
         })
@@ -359,6 +396,7 @@ export function looseEqual (a: any, b: any): boolean {
       return false
     }
   } else if (!isObjectA && !isObjectB) {
+    // 如果都不是 Object，则调用 String() 将它们都转成字符串判断是否相等
     return String(a) === String(b)
   } else {
     return false
@@ -369,6 +407,8 @@ export function looseEqual (a: any, b: any): boolean {
  * Return the first index at which a loosely equal value can be
  * found in the array (if value is a plain object, the array must
  * contain an object of the same shape), or -1 if it is not present.
+ * 利用上面的 looseEqual 方法找到在数组中与目标值 looseEqual 的对应元素的索引
+ * 不存在则返回 -1
  */
 export function looseIndexOf (arr: Array<mixed>, val: mixed): number {
   for (let i = 0; i < arr.length; i++) {
@@ -379,8 +419,10 @@ export function looseIndexOf (arr: Array<mixed>, val: mixed): number {
 
 /**
  * Ensure a function is called only once.
+ * 确保一个函数只能被调用一次
  */
 export function once (fn: Function): Function {
+  // 闭包，标识保存是否已被调用的
   let called = false
   return function () {
     if (!called) {
