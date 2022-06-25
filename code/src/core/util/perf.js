82618106242d8d35c2b2ea20f0de729cace7eb31
ScoutYin <scoutyin.1900@gmail.com>
2022-06-25 23:17:24 +0800

    add: 性能测定代码分析

--- code/src/core/util/perf.js
+++ code/src/core/util/perf.js
@@ -1,9 +1,13 @@
 import { inBrowser } from './env'
 
+// 导出的两个用于性能测定的方法
 export let mark
 export let measure
 
+// 在非生产环境下，判断在浏览器环境，并且支持原生 window.performance 对象
+// 才会有 mark 和 measure 方法，否则是 undefined
 if (process.env.NODE_ENV !== 'production') {
+  // 浏览器原生 performance 对象
   const perf = inBrowser && window.performance
   /* istanbul ignore if */
   if (
@@ -13,6 +17,7 @@ if (process.env.NODE_ENV !== 'production') {
     perf.clearMarks &&
     perf.clearMeasures
   ) {
+    // mark 方法内部调用 window.performance.mark 方法
     mark = tag => perf.mark(tag)
     measure = (name, startTag, endTag) => {
       perf.measure(name, startTag, endTag)
